2e883f466e7aad18a59ae96064bec1203c18f957
dutchenkoOleg <dutchenko.o.dev@gmail.com>
2020-10-23 01:06:40 +0300

    Extend rules

--- packages/eslint-config/.eslintrc.js
+++ packages/eslint-config/.eslintrc.js
@@ -1,4 +1,16 @@
 module.exports = {
 	extends: ['standard', 'plugin:prettier/recommended'],
-	rules: {}
+	rules: {
+		'no-tabs': 0,
+		'import/no-unresolved': 0,
+		'import/namespace': 0,
+		indent: [
+			'error',
+			'tab',
+			{
+				SwitchCase: 1
+			}
+		],
+		semi: ['error', 'always']
+	}
 };
